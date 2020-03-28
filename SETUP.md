bdd266e6c4d0000b2ca4a5bcd958d111320a5a4c
konabuta <keonabut@microsoft.com>
2020-03-28 12:21:18 +0000

    first

---
+++ SETUP.md
@@ -0,0 +1,169 @@
+# Setup Guide
+
+This document describes how to setup all the dependencies to run the notebooks in this repository.
+
+The recommended environment to run these notebooks is the [Azure Data Science Virtual Machine (DSVM)](https://azure.microsoft.com/en-us/services/virtual-machines/data-science-virtual-machines/). Since a considerable number of the algorithms rely on deep learning, it is recommended to use a GPU DSVM.
+
+For training at scale, operationalization or hyperparameter tuning, it is recommended to use [Azure ML](https://docs.microsoft.com/en-us/azure/machine-learning/service/).
+
+
+## Table of Contents
+
+* [Compute environments](#compute-environments)
+* [Create a cloud-based workstation (Optional)](#Create-a-cloud-based-workstation-optional)
+* [Setup guide for Local or Virtual Machines](#setup-guide-for-local-or-virtual-machines)
+  * [Requirements](#requirements)
+  * [Dependencies setup](#dependencies-setup)
+  * [Register the conda environment in the DSVM JupyterHub](#register-conda-environment-in-dsvm-jupyterhub)
+  * [Installing the Repo's Utils via PIP](#installing-the-repos-utils-via-pip)
+* [Setup guide for docker](#Set-up-guide-for-nvidia-docker)
+
+## Compute Environments
+
+Depending on the type of NLP system and the notebook that needs to be run, there are different computational requirements. Currently, this repository supports **Python CPU** and **Python GPU**. A conda environment YAML file can be generated for either CPU or GPU environments as shown below in the *Dependencies Setup* section.
+
+## Create a cloud-based workstation (Optional)
+
+[Azure Machine Learning service](https://azure.microsoft.com/en-us/services/machine-learning-service/)’s Notebook Virtual Machine (VM), is a cloud-based workstation created specifically for data scientists. Notebook VM based authoring is directly integrated into Azure Machine Learning service, providing a code-first experience for Python developers to conveniently build and deploy models in the workspace. Developers and data scientists can perform every operation supported by the Azure Machine Learning Python SDK using a familiar Jupyter notebook in a secure, enterprise-ready environment. Notebook VM is secure and easy-to-use, preconfigured for machine learning, and fully customizable. 
+
+You can learn how to create a Notebook VM [here](https://docs.microsoft.com/en-us/azure/machine-learning/service/tutorial-1st-experiment-sdk-setup#azure) and then follow the same setup as in the [Setup guide for Local or DSVM](#setup-guide-for-local-or-dsvm-machines) directly using the terminal in the Notebook VM.
+
+## Setup Guide for Local or Virtual Machines
+
+### Requirements
+
+* A machine running Linux, MacOS or Windows.
+* On Windows, Microsoft Visual C++ 14.0 is required for building certain packages. Download Microsoft Visual C++ Build Tools [here](https://visualstudio.microsoft.com/downloads/).
+
+* Miniconda or Anaconda with Python version >= 3.6.
+    * This is pre-installed on Azure DSVM such that one can run the following steps directly. To setup on your local machine, [Miniconda](https://docs.conda.io/en/latest/miniconda.html) is a quick way to get started.
+    * It is recommended to update conda to the latest version: `conda update -n base -c defaults conda`
+
+> NOTE: Windows machines are not **FULLY SUPPORTED**. Please use at your own risk.
+
+* Mecab と辞書 `mecab-ipadic-2.7.0-20070801` の導入
+     * Mecab のインストール
+        ```bash
+        sudo apt install mecab libmecab-dev mecab-ipadic-utf8
+        ```
+     * 辞書のダウンロード
+         * http://atilika.com/releases/mecab-ipadic/mecab-ipadic-2.7.0-20070801.tar.gz
+         * http://jaist.dl.sourceforge.net/project/mecab/mecab-ipadic/2.7.0-20070801/mecab-ipadic-2.7.0-20070801.tar.gz
+     * 辞書のインストール
+         ```bash
+         wget -O <上記 URL>
+         tar zxvf mecab-ipadic-2.7.0-20070801.tar.gz
+         cd mecab-ipadic-2.7.0-20070801
+         ./configure --with-charset=utf8 --enable-utf8-only
+         make
+         sudo make install
+         ```
+
+### Dependencies Setup
+
+
+We provide a script, [generate_conda_file.py](tools/generate_conda_file.py), to generate a conda-environment yaml file
+which you can use to create the target environment using the Python version 3.6 with all the correct dependencies.
+
+Assuming the repo is cloned as `nlp-recipes` in the system, to install **a default (Python CPU) environment**:
+
+    cd nlp-recipes
+    python tools/generate_conda_file.py
+    conda env create -f nlp_cpu.yaml
+
+You can specify the environment name as well with the flag `-n`.
+
+Click on the following menus to see how to install the Python GPU environment:
+
+<details>
+<summary><strong><em>Python GPU environment on Linux, MacOS</em></strong></summary>
+
+Assuming that you have a GPU machine, to install the Python GPU environment, which by default installs the CPU environment:
+
+    cd nlp-recipes
+    python tools/generate_conda_file.py --gpu
+    conda env create -n nlp_gpu -f nlp_gpu.yaml
+
+</details>
+
+<details>
+<summary><strong><em>Python GPU environment on Windows</em></strong></summary>
+
+Assuming that you have an Azure GPU DSVM machine, here are the steps to setup the Python GPU environment:
+1. Make sure you have CUDA Toolkit version 9.0 above installed on your Windows machine. You can run the command below in your terminal to check.
+
+         nvcc --version
+    If you don't have CUDA Toolkit or don't have the right version, please download it from here: [CUDA Toolkit](https://developer.nvidia.com/cuda-toolkit)
+
+2. Install the GPU environment.
+
+        cd nlp-recipes
+        python tools/generate_conda_file.py --gpu
+        conda env create -n nlp_gpu -f nlp_gpu.yaml
+
+</details>
+
+### Register Conda Environment in DSVM JupyterHub
+
+We can register our created conda environment to appear as a kernel in the Jupyter notebooks.
+
+    conda activate my_env_name
+    python -m ipykernel install --user --name my_env_name --display-name "Python (my_env_name)"
+
+If you are using the DSVM, you can [connect to JupyterHub](https://docs.microsoft.com/en-us/azure/machine-learning/data-science-virtual-machine/dsvm-ubuntu-intro#jupyterhub-and-jupyterlab) by browsing to `https://your-vm-ip:8000`.  If you are prompted to enter user name and password, enter the user name and password that you use to log in to your virtual machine. 
+
+### Installing the Repo's Utils via PIP
+
+<details>
+    <summary>The utils_nlp module of this repository needs to be installed as a python package in order to be used by the examples. <strong><em>Click to expand and see the details</em></strong> 
+    </summary> 
+    <p>  
+A setup.py file is provided in order to simplify the installation of this utilities in this repo from the main directory.  
+    
+To install the package, please run the command below (from directory root)
+
+    pip install -e . 
+
+Running the command tells pip to install the `utils_nlp` package from source in [development mode](https://setuptools.readthedocs.io/en/latest/setuptools.html#development-mode). This just means that any updates to `utils_nlp` source directory will immediately be reflected in the installed package without needing to reinstall; a very useful practice for a package with constant updates.   
+
+> It is also possible to install directly from Github, which is the best way to utilize the `utils_nlp` package in external projects (while still reflecting updates to the source as it's installed as an editable `'-e'` package). 
+
+>   `pip install -e  git+git@github.com:microsoft/nlp-recipes.git@master#egg=utils_nlp`  
+
+Either command, from above, makes `utils_nlp` available in your conda virtual environment. You can verify it was properly installed by running:  
+
+    pip list  
+    
+
+**NOTE** - The pip installation does not install any of the necessary package dependencies, it is expected that conda will be used as shown above to setup the environment for the utilities being used.
+    </p>
+</details>
+
+The details of the versioning info can be found at [VERSIONING.md](VERSIONING.md).
+
+# Set up guide for (nvidia) docker
+
+## Pre-requisites
+In order to use the notebooks within a docker enviornment, you will need to have [nvidia docker drivers](https://github.com/NVIDIA/nvidia-docker) and [docker](https://docs.docker.com/install/linux/docker-ce/ubuntu/) installed on your computer.
+
+## Building docker image
+A docker file is provided within the [docker](docker/) folder. You can create the image using 
+```
+  cd docker
+  docker build -f . -t nlp-recipes
+```
+This will create a docker image containing all the dependencies and will name it as nlp-recipies:latest
+
+## Running the container
+You can run the notebook within the container environment using
+```
+  docker run --gpus all -p 8888:8888 nlp-recipes
+```
+This will map port 8888 of the local machine 
+
+## Trouble shooting
+* If you have permission issues with `docker build` or `docker run`, you might need to run docker with sudo permissions. 
+* If you are getting 'port already in use' errors, consider mapping a different port on the local machine to port 8888 on the container e.g.
+```
+docker run --gpus all -p 9000:8888 nlp-recipes
+```
---
+++ utils_nlp/models/transformers/common.py
@@ -0,0 +1,364 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+# This script reuses some code from
+# https://github.com/huggingface/pytorch-transformers/blob/master/examples/run_glue.py
+
+import datetime
+import logging
+import os
+import random
+import time
+
+import numpy as np
+import torch
+from tqdm import tqdm
+
+#import transformers
+from transformers import AdamW, get_linear_schedule_with_warmup
+from transformers.modeling_bert import BERT_PRETRAINED_MODEL_ARCHIVE_MAP
+from transformers.modeling_distilbert import DISTILBERT_PRETRAINED_MODEL_ARCHIVE_MAP
+from transformers.modeling_roberta import ROBERTA_PRETRAINED_MODEL_ARCHIVE_MAP
+from transformers.modeling_xlnet import XLNET_PRETRAINED_MODEL_ARCHIVE_MAP
+from transformers.tokenization_bert import BertTokenizer
+from transformers.tokenization_distilbert import DistilBertTokenizer
+from transformers.tokenization_roberta import RobertaTokenizer
+from transformers.tokenization_xlnet import XLNetTokenizer
+from transformers.tokenization_bert_japanese import BertJapaneseTokenizer
+
+from utils_nlp.common.pytorch_utils import (
+    get_device,
+    move_model_to_device,
+    get_amp,
+    parallelize_model,
+)
+
+TOKENIZER_CLASS = {}
+TOKENIZER_CLASS.update({k: BertTokenizer for k in BERT_PRETRAINED_MODEL_ARCHIVE_MAP})
+TOKENIZER_CLASS.update(
+    {k: RobertaTokenizer for k in ROBERTA_PRETRAINED_MODEL_ARCHIVE_MAP}
+)
+TOKENIZER_CLASS.update({k: XLNetTokenizer for k in XLNET_PRETRAINED_MODEL_ARCHIVE_MAP})
+TOKENIZER_CLASS.update(
+    {k: DistilBertTokenizer for k in DISTILBERT_PRETRAINED_MODEL_ARCHIVE_MAP}
+)
+
+# 日本語対応
+TOKENIZER_CLASS.update({k: BertJapaneseTokenizer for k in BERT_PRETRAINED_MODEL_ARCHIVE_MAP})
+
+
+MAX_SEQ_LEN = 512
+
+logger = logging.getLogger(__name__)
+
+
+class Transformer:
+    def __init__(
+        self,
+        model_class,
+        model_name="bert-base-cased",
+        num_labels=2,
+        cache_dir=".",
+        load_model_from_dir=None,
+    ):
+
+        if model_name not in self.list_supported_models():
+            raise ValueError(
+                "Model name {0} is not supported by {1}. "
+                "Call '{1}.list_supported_models()' to get all supported model "
+                "names.".format(model_name, self.__class__.__name__)
+            )
+        self._model_name = model_name
+        self._model_type = model_name.split("-")[0]
+        self.cache_dir = cache_dir
+        self.load_model_from_dir = load_model_from_dir
+        if load_model_from_dir is None:
+            self.model = model_class[model_name].from_pretrained(
+                model_name,
+                cache_dir=cache_dir,
+                num_labels=num_labels,
+                output_loading_info=False,
+            )
+        else:
+            logger.info("Loading cached model from {}".format(load_model_from_dir))
+            self.model = model_class[model_name].from_pretrained(
+                load_model_from_dir, num_labels=num_labels, output_loading_info=False
+            )
+
+    @property
+    def model_name(self):
+        return self._model_name
+
+    @property
+    def model_type(self):
+        return self._model_type
+
+    @staticmethod
+    def set_seed(seed, cuda=True):
+        random.seed(seed)
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+        if cuda and torch.cuda.is_available():
+            torch.cuda.manual_seed_all(seed)
+
+    @staticmethod
+    def get_default_optimizer(model, weight_decay, learning_rate, adam_epsilon):
+        no_decay = ["bias", "LayerNorm.weight"]
+        optimizer_grouped_parameters = [
+            {
+                "params": [
+                    p
+                    for n, p in model.named_parameters()
+                    if not any(nd in n for nd in no_decay)
+                ],
+                "weight_decay": weight_decay,
+            },
+            {
+                "params": [
+                    p
+                    for n, p in model.named_parameters()
+                    if any(nd in n for nd in no_decay)
+                ],
+                "weight_decay": 0.0,
+            },
+        ]
+        optimizer = AdamW(
+            optimizer_grouped_parameters, lr=learning_rate, eps=adam_epsilon
+        )
+        return optimizer
+
+    @staticmethod
+    def get_default_scheduler(optimizer, warmup_steps, num_training_steps):
+        scheduler = get_linear_schedule_with_warmup(
+            optimizer,
+            num_warmup_steps=warmup_steps,
+            num_training_steps=num_training_steps,
+        )
+        return scheduler
+
+    def prepare_model_and_optimizer(
+        self,
+        num_gpus,
+        gpu_ids,
+        local_rank,
+        weight_decay,
+        learning_rate,
+        adam_epsilon,
+        fp16=False,
+        fp16_opt_level="O1",
+        checkpoint_state_dict=None,
+    ):
+        """
+        This function initializes an optimizer and moves the model to a device.
+        It can be used by most child classes before calling fine_tune.
+        Child classes that require custom optimizers need to either override this
+            function or implement the steps listed below in the specified order
+            before fine-tuning.
+
+        The steps are performed in the following order:
+            1. Move model to device
+            2. Create optimizer
+            3. Initialize amp
+            4. Parallelize model
+        """
+
+        amp = get_amp(fp16)
+
+        # get device
+        device, num_gpus = get_device(
+            num_gpus=num_gpus, gpu_ids=gpu_ids, local_rank=local_rank
+        )
+
+        # move model
+        self.model = move_model_to_device(model=self.model, device=device)
+
+        # init optimizer
+        self.optimizer = Transformer.get_default_optimizer(
+            self.model, weight_decay, learning_rate, adam_epsilon
+        )
+
+        if fp16:
+            self.model, self.optimizer = amp.initialize(
+                self.model, self.optimizer, opt_level=fp16_opt_level
+            )
+
+        if checkpoint_state_dict:
+            self.optimizer.load_state_dict(checkpoint_state_dict["optimizer"])
+            self.model.load_state_dict(checkpoint_state_dict["model"])
+
+            if fp16:
+                amp.load_state_dict(checkpoint_state_dict["amp"])
+
+        self.model = parallelize_model(
+            model=self.model,
+            device=device,
+            num_gpus=num_gpus,
+            gpu_ids=gpu_ids,
+            local_rank=local_rank,
+        )
+
+        return device, num_gpus, amp
+
+    def fine_tune(
+        self,
+        train_dataloader,
+        get_inputs,
+        device,
+        num_gpus=None,
+        max_steps=-1,
+        global_step=0,
+        max_grad_norm=1.0,
+        gradient_accumulation_steps=1,
+        optimizer=None,
+        scheduler=None,
+        fp16=False,
+        amp=None,
+        local_rank=-1,
+        verbose=True,
+        seed=None,
+        report_every=10,
+        save_every=-1,
+        clip_grad_norm=True,
+    ):
+
+        if seed is not None:
+            Transformer.set_seed(seed, num_gpus > 0)
+
+        # init training
+        tr_loss = 0.0
+        accum_loss = 0
+        self.model.train()
+        self.model.zero_grad()
+
+        # train
+        start = time.time()
+        while global_step < max_steps:
+            epoch_iterator = tqdm(
+                train_dataloader,
+                desc="Iteration",
+                disable=local_rank not in [-1, 0] or not verbose,
+            )
+            for step, batch in enumerate(epoch_iterator):
+                inputs = get_inputs(batch, device, self.model_name)
+                outputs = self.model(**inputs)
+                loss = outputs[0]
+
+                if num_gpus > 1:
+                    loss = loss.mean()
+                if gradient_accumulation_steps > 1:
+                    loss = loss / gradient_accumulation_steps
+
+                if fp16:
+                    with amp.scale_loss(loss, optimizer) as scaled_loss:
+                        scaled_loss.backward()
+                else:
+                    loss.backward()
+
+                tr_loss += loss.item()
+                accum_loss += loss.item()
+
+                if (step + 1) % gradient_accumulation_steps == 0:
+                    global_step += 1
+
+                    if clip_grad_norm:
+                        if fp16:
+                            torch.nn.utils.clip_grad_norm_(
+                                amp.master_params(optimizer), max_grad_norm
+                            )
+                        else:
+                            torch.nn.utils.clip_grad_norm_(
+                                self.model.parameters(), max_grad_norm
+                            )
+
+                    if global_step % report_every == 0 and verbose:
+                        end = time.time()
+                        endtime_string = datetime.datetime.fromtimestamp(end).strftime(
+                            "%d/%m/%Y %H:%M:%S"
+                        )
+                        print(
+                            """timestamp: {0:s}, loss: {1:.6f}, time duration: {2:f},
+                            number of examples in current step: {3:.0f}, step {4:.0f}
+                            out of total {5:.0f}""".format(
+                                endtime_string,
+                                accum_loss / report_every,
+                                end - start,
+                                len(batch),
+                                global_step,
+                                max_steps,
+                            )
+                        )
+                        accum_loss = 0
+                        start = end
+
+                    optimizer.step()
+                    if scheduler:
+                        scheduler.step()
+                    self.model.zero_grad()
+                    if save_every != -1 and global_step % save_every == 0 and verbose:
+                        self.save_model(
+                            os.path.join(
+                                self.cache_dir,
+                                f"{self.model_name}_step_{global_step}.pt",
+                            )
+                        )
+                if global_step > max_steps:
+                    epoch_iterator.close()
+                    break
+
+        return global_step, tr_loss / global_step
+
+    def predict(self, eval_dataloader, get_inputs, num_gpus, gpu_ids, verbose=True):
+        # get device
+        device, num_gpus = get_device(num_gpus=num_gpus, gpu_ids=gpu_ids, local_rank=-1)
+
+        # move model
+        self.model = move_model_to_device(model=self.model, device=device)
+
+        # parallelize model
+        self.model = parallelize_model(
+            model=self.model,
+            device=device,
+            num_gpus=num_gpus,
+            gpu_ids=gpu_ids,
+            local_rank=-1,
+        )
+
+        # predict
+        self.model.eval()
+        for batch in tqdm(eval_dataloader, desc="Scoring", disable=not verbose):
+            with torch.no_grad():
+                inputs = get_inputs(batch, device, self.model_name, train_mode=False)
+                outputs = self.model(**inputs)
+                logits = outputs[0]
+            yield logits.detach().cpu().numpy()
+
+    def save_model(self, full_name=None):
+        """
+        save the trained model.
+
+        Args:
+            full_name (str, optional): File name to save the model's `state_dict()`
+                that can be loaded by torch.load().
+                If None, the trained model, configuration and tokenizer are saved
+                using `save_pretrained()`; and the file is going to be saved under
+                "fine_tuned" folder of the cached directory of the object.
+                Defaults to None.
+        """
+
+        # Save a trained model, configuration and tokenizer using `save_pretrained()`.
+        # They can then be reloaded using `from_pretrained()`
+        model_to_save = (
+            self.model.module if hasattr(self.model, "module") else self.model
+        )  # Take care of distributed/parallel training
+
+        if full_name:
+            logger.info("Saving model checkpoint to %s", full_name)
+            torch.save(model_to_save.state_dict(), full_name)
+        else:
+            output_model_dir = os.path.join(self.cache_dir, "fine_tuned")
+
+            os.makedirs(self.cache_dir, exist_ok=True)
+            os.makedirs(output_model_dir, exist_ok=True)
+
+            logger.info("Saving model checkpoint to %s", output_model_dir)
+            model_to_save.save_pretrained(output_model_dir)
